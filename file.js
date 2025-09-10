85940cf383a02a879c634261f0f7bfc616e9f193
triveni898 <settytriveni745@gmail.com>
2025-09-10 22:33:12 +0530

    Update file.js

--- file.js
+++ file.js
@@ -1,10 +1,11 @@
 console.log("Triveni")
 let trainer = "Setty Triveni"
 let subject ="Github"
-let duration ="2 hrs"
+let duration ="2.5 hrs"
 let anothertrainer = "Yamini"
 let anothersubject ="Git VCS"
 let topicname ="conflict"
 console.log("Github session")
 console.log("Welcome to Github Session")
 console.log("Hi Nanna")
+console.log(another trainer)
