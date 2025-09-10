a054578e2d4a7b38044094dd0a46ab8793bd5aa2
Setty Triveni <settytriveni745@.com>
2025-09-10 13:17:08 +0530

    Merged with latest

--- file.js
+++ file.js
@@ -1,7 +1,5 @@
 console.log("Triveni")
 let trainer = "Triveni"
-<<<<<<< HEAD
 let subject ="Github"
-=======
 let duration ="2 hrs"
->>>>>>> b391d89340e122314360803cb29079f8f09696bc
+let anothertrainer = "Yamini"
